79d36539d07d86fea3ed53c08744c92fee83b234
Pasha0666 <soldatov00009@gmail.com>
2020-01-15 15:13:12 +0500

    работающий

--- main/main.ino
+++ main/main.ino
@@ -4,10 +4,16 @@
 
 const int echoPin = 8;
 const int trigPin = 9;
-MyServo myServo(PIN_SERVO);
+//MyServo myServo(PIN_SERVO);
+Servo servo;
+
+int angle;
+bool plus;
 
 void setup()
 {
+  angle = 0;
+  servo.attach(PIN_SERVO);
   pinMode(echoPin, INPUT);
   pinMode(trigPin, OUTPUT);
   digitalWrite(trigPin, LOW);
@@ -15,8 +21,16 @@ void setup()
 }
 
 void loop() {
-  myServo.attack();
-  delay(100);
+  float result = readDist();
+  Serial.println(result);
+  for (int i = 0;i < 5;i++)
+  {
+  if (result < 30.0)
+    attack();
+  else
+    stopAttack();
+  delay(30);
+  }
 }
 
 float readDist()
@@ -24,6 +38,27 @@ float readDist()
   const float speedOfSoundMPerSec = 340.0;
   const float speedOfSoundCmPerUs = speedOfSoundMPerSec / 10000.0;
   return readPulse() * speedOfSoundCmPerUs / 2.0;    
+}
+
+void attack()
+{
+  if (angle > 90 || angle < 0)
+    plus = !plus;
+      
+  if (plus)
+    angle += 15;
+  else
+    angle -= 15;
+  servo.write(angle);
+}
+
+void stopAttack()
+{
+  if (angle != 0)
+  {
+    angle = 0;
+    servo.write(0);
+  }
 }
 
 float readPulse()
